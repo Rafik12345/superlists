6c22e38fc27a8f4ce24b71db039ba2247366c576
rafik12345 <rafik-08@hotmail.com>
2020-02-19 14:17:19 +0100

    Enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,7 +12,7 @@ class ItemValidationTest(FunctionalTest):
 		
 		# The home page refreshes, and there is an error message saying that list items cannot be blank
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text, "You can't have a empty list item"))
+			self.browser.find_element_by_css_selector('.has-error').text, "You can't have an empty list item"))
 		
 		# She tries again with some text for the item, which now works
 		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
@@ -24,10 +24,11 @@ class ItemValidationTest(FunctionalTest):
 		
 		# she receives a similar warning on the list page
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text, "You can't have a empty list item"))
+			self.browser.find_element_by_css_selector('.has-error').text, "You can't have an empty list item"))
 		
 		# and she can corret it by filling some text
-		self.find_element_by_id('id_new_item').send_keys('Make tea')
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
 		self.wait_for_row_in_list_table('2: Make tea')
 		
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -63,7 +63,15 @@ class ListViewTest(TestCase):
 			data={'item_text': 'A new item for an existing list'}
 			)
 
-		self.assertRedirects(response, f'/lists/{correct_list.id}/')							
+		self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+	def test_validation_error_end_up_on_list_page(self):
+		list_ = List.objects.create()
+		response = self.client.post(f"/lists/{list_.id}/", data={'item_text': ''})
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, "list.html")
+		expected_error = escape("You can't have an empty list item")
+		self.assertContains(response, expected_error)
 
 
 class NewListTest(TestCase):
@@ -82,7 +90,7 @@ class NewListTest(TestCase):
 		response = self.client.post('/lists/new', data={'item_text': ''})
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'home.html')
-		expected_error = escape("You can't have a empty list item")
+		expected_error = escape("You can't have an empty list item")
 		self.assertContains(response, expected_error)
 
 	def test_invalid_list_items_arent_saved(self):
