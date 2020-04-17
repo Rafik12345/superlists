c58bf812a1dbab9c69e9871a1b484b0d21e502ae
rafik12345 <rafik-08@hotmail.com>
2020-04-17 15:40:01 +0200

    End CH20

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -17,9 +17,25 @@ class FunctionalTest(StaticLiveServerTestCase):
 		if staging_server:
 			self.live_server_url = 'http://' + staging_server
 
-	def tearDown(self):
-		self.browser.quit()	
 
+	def tearDown(self):
+		self.browser.quit()
+
+
+	def wait(fn):
+		def modified_fn(*args, **kwargs):
+			start_time = time.time()
+			while True:
+				try:
+					return fn(*args, **kwargs)
+				except (AssertionError, WebDriverException) as e:
+					if time.time - start_time > MAX_WAIT:
+						raise e
+					time.sleep(0.5)
+		return modified_fn				
+
+
+	@wait
 	def wait_for_row_in_list_table(self, row_text):
 		start_time = time.time()
 		while True:
@@ -34,21 +50,12 @@ class FunctionalTest(StaticLiveServerTestCase):
 				time.sleep(0.5)
 
 	
+	@wait
 	def wait_for(self, fn):
-		start_time = time.time()
-		while True:
-			try:	
-				return fn()
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
+		return fn()
 
-	
-	def get_item_input_box(self):
-		return self.browser.find_element_by_id('id_text')
 
-	
+	@wait
 	def wait_to_be_logged_in(self, email):
 		self.wait_for(
 			lambda: self.browser.find_element_by_link_text('Log out')
@@ -57,7 +64,12 @@ class FunctionalTest(StaticLiveServerTestCase):
 		self.assertIn(email, navbar.text)
 
 
+	@wait
 	def wait_to_be_logged_out(self, email):						
 		self.wait_for(lambda:
 			self.browser.find_element_by_name('email')
 		)
+
+
+	def get_item_input_box(self):
+		return self.browser.find_element_by_id('id_text')	
--- superlists/settings.py
+++ superlists/settings.py
@@ -55,11 +55,13 @@ AUTHENTICATION_BACKENDS = [
     'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hazetestphp9@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
