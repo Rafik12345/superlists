486af999e05fe718a717966dbb4a54d4d3db68f6
rafik12345 <rafik-08@hotmail.com>
2020-04-25 16:49:29 +0200

    LOGGING Settings for server

--- superlists/settings.py
+++ superlists/settings.py
@@ -12,6 +12,23 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
